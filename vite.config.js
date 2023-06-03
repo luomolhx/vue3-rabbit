e4dec002a12f2fdc6a6921f6f318d02263b7ce53
luomolhx <2932965369@qq.com>
2023-06-03 16:01:55 +0800

    elementPlus主题色的定制

--- vite.config.js
+++ vite.config.js
@@ -12,17 +12,32 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(),
-    // 
+    //
     AutoImport({
-      resolvers:[ElementPlusResolver()]
+      resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers:[ElementPlusResolver()]
+      // 1. 配置elementPlus采用sass样式配色系统
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass'
+        })
+      ]
     })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2. 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `
+      }
     }
   }
 })
