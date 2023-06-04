046da38c16fb3a2809764b5175ac7fd85b864b05
luomolhx <2932965369@qq.com>
2023-06-04 11:02:37 +0800

    home-goodsItem组件封装

--- src/directives/index.js
+++ src/directives/index.js
@@ -7,7 +7,6 @@ export const lazyPlugin = {
         // el: 指令绑定的那个元素
         // binding: binding.value 指令等于号后面绑定的表达式的值 图片url
         const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
-          console.log(isIntersecting)
           if (isIntersecting) {
             // 进入视窗区域
             el.src = binding.value
