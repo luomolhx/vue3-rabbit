57eb488f4aed49da39c9731185f1d74c66538045
luomolhx <2932965369@qq.com>
2023-06-04 09:02:23 +0800

    home-整体结构拆分和分类数据渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,8 +1,17 @@
-<script></script>
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
+</script>
 
 <template>
-  <div>我是Home页</div>
-  <div style="height: 1200ox"></div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
 </template>
-
-<style></style>
